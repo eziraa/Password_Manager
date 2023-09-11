9a58aa4c8ace9f78b7cbacbfde78f9c9ead7a62f
sutual <ezirayallew@gmail.com>
2023-09-11 15:28:20 +0300

    Password Manager is going to finish

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ window.minsize(500, 500)
 window.title("Password Manager")
 window.config(padx=20, pady=20)
 window.config(bg="white")
+window.rowconfigure(1, pad=20)
+window.rowconfigure(3, pad=20)
+window.columnconfigure(2, pad=20)
+window.columnconfigure(1, pad=20)
 
 
 def generatePassword():
@@ -30,13 +34,13 @@ def generatePassword():
 
 def is_valid_inputs(website, email, password):
     if website == "":
-        messagebox.showerror(title="Warning", message="The website field is empty")
+        messagebox.showwarning(title="Warning", message="The website field is empty")
         return False
     elif email == "":
-        messagebox.showerror(title="Warning", message="The email field is empty")
+        messagebox.showwarning(title="Warning", message="The email field is empty")
         return False
     elif password == "":
-        messagebox.showerror(title="Warning", message="The password field is empty")
+        messagebox.showwarning(title="Warning", message="The password field is empty")
         return False
     else:
         return True
@@ -52,25 +56,37 @@ def save():
     }}
     if is_valid_inputs(website=website, email=email, password=password):
         try:
-            with open("data.json", "r") as file_read:
-                # Loading data
-                data = json.load(file_read)
-                # Updating data
-                data.update(to_be_written)
+            with open("data.json", "r") as data_file:
+                # Reading data
+                data = json.load(data_file)
         except FileNotFoundError:
-            with open("data.json", "w") as file_write:
-                json.dump(to_be_written, file_write, indent=4)
-                web_name_entry.delete(0, END)
-                password_entry.delete(0, END)
-                web_name_entry.focus()
-                messagebox.showinfo("Show successes", message="The information in saved successfully")
+            with open("data.json", "w") as data_file:
+                json.dump(to_be_written, data_file, indent=4)
         else:
-            with open("data.json", "w") as file_write:
-                json.dump(data, file_write, indent=4)
-                web_name_entry.delete(0, END)
-                password_entry.delete(0, END)
-                web_name_entry.focus()
-                messagebox.showinfo("Show successes", message="The information in saved successfully")
+            # Updating data
+            data.update(to_be_written)
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
+            web_name_entry.delete(0, END)
+            password_entry.delete(0, END)
+            web_name_entry.focus()
+            messagebox.showinfo("Show successes", message="The information in saved successfully")
+
+
+def find_password():
+    website = web_name_entry.get()
+    try:
+        with open("data.json", "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showwarning(title="Errno", message="There is no data file found")
+    else:
+        try:
+            messagebox.showinfo(title=website,
+                                message=f"Email: {data[website]['email']} \nPassword: {data[website]['password']}")
+        except KeyError:
+            messagebox.showinfo(title=website, message="There is no password saved for the website")
 
 
 image = PhotoImage(file="logo.png")
@@ -87,17 +103,19 @@ password_label = Label(text="Password", bg="white")
 password_label.grid(row=3, column=0)
 
 # Entries
-web_name_entry = Entry(width=50)
-web_name_entry.grid(row=1, column=1, columnspan=2)
-email_entry = Entry(width=50)
+web_name_entry = Entry(width=30)
+web_name_entry.grid(row=1, column=1)
+email_entry = Entry(width=59)
 email_entry.grid(row=2, column=1, columnspan=2)
 email_entry.insert(0, "ezra@gmail.com")
 password_entry = Entry(width=30)
 password_entry.grid(row=3, column=1)
 # Buttons
-generate_btn = Button(text="Generate Password", width=16, command=generatePassword)
+generate_btn = Button(text="Generate Password", width=20, command=generatePassword)
 generate_btn.grid(row=3, column=2)
-add_btn = Button(text="Add", width=42, command=save)
+add_btn = Button(text="Add", width=50, command=save)
 add_btn.grid(row=4, column=1, columnspan=2)
+search_btn = Button(text="Search", command=find_password, width=20)
+search_btn.grid(row=1, column=2)
 
 window.mainloop()
